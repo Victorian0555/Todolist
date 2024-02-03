879e784cc40e95bfd49d5fd0f606de7de8a2ba20
Victoria <158543805+Victorian0555@users.noreply.github.com>
2024-02-03 12:54:49 +0300

    Update Todolist.py

--- Todolist.py
+++ Todolist.py
@@ -1,8 +1,9 @@
 from datetime import date
 
 def create_todo_list():
-    current_date = date.today()
+    current_date = date.today().strftime("%Y-%m-%d")
     tasks = []
+    print("- - - - - - TO - DO - LIST - - - - - -")
     print("Введите задачи (для завершения введите пустую строку):")
     while True:
         task = input()
@@ -11,8 +12,9 @@ def create_todo_list():
         tasks.append(task)
     filename = f"todo_list_{current_date}.txt"
     with open(filename, "w") as file:
-        for task in tasks:
-            file.write(task + "\n")
+        file.write("- - - - - - TO - DO - LIST - - - - - -\n")
+        for i, task in enumerate(tasks, 1):
+            file.write(f"{i}. {task}\n")
     print(f"Файл {filename} успешно создан!")
 
 if __name__ == "__main__":
